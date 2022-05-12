f7147ce5b092377499ae377f16c6ae895dc9ab5c
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-05-12 05:13:35 +0400

    Update products.service.js

--- frontend/src/services/products.service.js
+++ frontend/src/services/products.service.js
@@ -4,6 +4,9 @@ class ProductService {
   getTrendingProducts(page, limit, refresh) {
     return api.get('/products/trending', { params: { page, limit, refresh } });
   }
+  getSuggestions(prefix) {
+    return api.get('/products/suggestions', { params: { prefix } });
+  }
 }
 
 export default new ProductService();
