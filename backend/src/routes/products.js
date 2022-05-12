026860d146ad506a7585f42d87cef4d1931b06ac
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-05-12 05:13:22 +0400

    create route for suggestions

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productController = require('../controllers/products');
 
 router.get('/trending', productController.getTrendingProducts);
+router.get('/suggestions', productController.getSuggestions)
 
 module.exports = router;
