9f66f50633a435e8ed53e3af6373783fff430c3b
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-05-12 05:13:12 +0400

    create controller for suggestions

--- backend/src/controllers/products.js
+++ backend/src/controllers/products.js
@@ -1,19 +1,25 @@
 const TrendingProduct = require('../models/trendingProduct');
 const createTrendingProductsMaterialView = require('../util/createTrendingProductsMaterialView');
+const cache = require('../util/cache');
 
 async function getTrendingProducts(req, res, next) {
   try {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const refresh = req.query.refresh;
+    const search = req.query.search;
 
     // This is very important to note. The materialized view is triggered to refresh by the refresh query parameter.
     // This is not good practice if you have multiple users accessing the database.
     // This is because the materialized view will be triggered multiple times.
     if (refresh) await createTrendingProductsMaterialView();
 
+    let query = {}
+    if (search) query.$text = { $search: search };
+
     // Pagination logic
-    let trendingProducts = await TrendingProduct.find()
+    let trendingProducts = await TrendingProduct
+      .find(query)
       .skip((page - 1) * limit)
       .limit(limit)
       .exec();
@@ -30,8 +36,22 @@ async function getTrendingProducts(req, res, next) {
   }
 }
 
+async function getSuggestions(req, res, next) {
+  try {
+    const prefix = req.query.prefix;
+    const autoSuggestions = cache.get('trie').autoSuggestions(prefix, 10);
+    return res.status(200).json({
+      autoSuggestions,
+    })
+  } catch (err) {
+    console.error(err.message);
+    next(err);
+  }
+}
+
 const productController = {
   getTrendingProducts,
+  getSuggestions,
 };
 
 module.exports = productController;
